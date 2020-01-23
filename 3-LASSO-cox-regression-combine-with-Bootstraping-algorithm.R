15901284a026e28a772223d058324677cc35c29f
Dongqiang Zeng <dongqiangzeng0808@gmail.com>
2020-01-23 14:51:21 +0800

    Add files via upload

--- 3-LASSO-cox-regression-combine-with-Bootstraping-algorithm.R
+++ 3-LASSO-cox-regression-combine-with-Bootstraping-algorithm.R
@@ -12,17 +12,10 @@ library(ggplot2)
 library(survival)
 #########################################
 #'load-filtered-features
-(load(paste0(path_training,"1-features.RData")))
-###########################################
-(load(paste0(path_training,"2-pdata-target-signature-matrix.RData")))
+(load("1-OS-and-filtered-signatures-matrix.RData"))
 ############################################
-target_eset[1:7,1:8]
-target_eset<-column_to_rownames(target_eset,var = "ID")
-######################################
-esetlasso<-target_eset[,c("status","time",fea_names)]
-######################################
-######################################
-dim(esetlasso);colnames(esetlasso)[1:50]
+sig_eset[1:7,1:8]
+##################################
 ##################################
 #bootstraping combine with LASSO cox regression
 ##################################
@@ -30,9 +23,9 @@ dim(esetlasso);colnames(esetlasso)[1:50]
 ##################################
 cao<-as.character()
 for(i in 1:10000){
-  a<-floor(runif(floor(dim(esetlasso)[1])*0.80,1,dim(esetlasso)[1]))
-  rt<-as.matrix(esetlasso[a,1:2])
-  genedatasd<-as.matrix(as.data.frame(esetlasso[a,3:ncol(esetlasso)]))
+  a<-floor(runif(floor(dim(sig_eset)[1])*0.80,1,dim(sig_eset)[1]))
+  rt<-as.matrix(sig_eset[a,1:2])
+  genedatasd<-as.matrix(as.data.frame(sig_eset[a,3:ncol(sig_eset)]))
   fit2<-cv.glmnet(genedatasd, rt, family="cox", maxit=1000,nfolds = 10,alpha=1)
   myCoefs <- coef(fit2, s="lambda.min");
   b<-myCoefs@Dimnames[[1]][which(myCoefs != 0 )]
